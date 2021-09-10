df209ec4a0ecff3f1ec64c7c9872724c50daf484
Bo Flachs <b.l.flachs@students.uu.nl>
2021-09-10 14:38:40 +0200

    Add loadFromFile function to Scenario

--- src/jaggpy/classes.py
+++ src/jaggpy/classes.py
@@ -10,43 +10,124 @@ from abc import ABC, abstractmethod
 
 class Scenario:
 	def __init__(self):
-		self.agenda = []
+		"""A Scenario object has the following properties:
+			- agenda: a list of formulas
+			- inputConstraints: a list of input constraints
+			- outputConstraints: a list of output constraints
+			- profile: a list of judgement sets """
+		self.agenda = dict()
 		self.inputConstraints = []
 		self.outputConstraints = []
-		self.profile = []
+		self.profile = dict()
 
 	def addToAgenda(self, formula):
-		self.agenda.append(formula)
-
-	def addToInputConstraints(self, constraint):
-		self.inputConstraints.append(constraint)
-
-	def addToOutputConstraints(self, constraint):
-		self.outputConstraints.append(constraint)
-
-	def addToProfile(self, judgementSet):
-		self.profile.append(judgementSet)
-
-	def loadFromFile(self, file):
-		"""Load the scenario from a .jagg file. The file should
-		have the following format, with each element being on 
-		a new line:
-			- Number of Formulas: The number of formulas in the pre-agenda
-			- X, Formula: The formula labeled by the number X  
-			- In, Formula: The input constraint labeled by the text "In"
-			- Out, Formula: The output constraint labeled by the text "Out"
-			- Number of Judgement Sets: The total number of judgement sets
-			- J, [Phi_1,...,Phi_n]: A list of the formulas phi_1 to phi_n 
-				that are accepted labelled by the number J. The formulas 
-				should be given by their label. The rest is rejected.
+		"""The addToAgenda function takes a formula as its only argument.
 		A formula should be build by lowercase atoms and the following operators:
 			- not
 			- and 
 			- or
 		The outermost parentheses can be omitted, but internal parentheses must
 		be explicit. For example, "(not x1 or not x2) or not x3" is the correct
+		format, while "not x1 or not x2 or not x3 will" not work. """
+		newLabel = len(self.agenda)+1
+		self.agenda[newLabel] = formula
+
+	def addToInputConstraints(self, constraint):
+		"""The addToInputConstraints function takes a formula as its only argument.
+		A formula should be build by lowercase atoms and the following operators:
+			- not
+			- and 
+			- or
+		The outermost parentheses can be omitted, but internal parentheses must
+		be explicit. For example, "(not x1 or not x2) or not x3" is the correct
+		format, while "not x1 or not x2 or not x3 will" not work. """
+		self.inputConstraints.append(constraint)
+
+	def addToOutputConstraints(self, constraint):
+		"""The addToOutputConstraints function takes a formula as its only argument.
+		A formula should be build by lowercase atoms and the following operators:
+			- not
+			- and 
+			- or
+		The outermost parentheses can be omitted, but internal parentheses must
+		be explicit. For example, "(not x1 or not x2) or not x3" is the correct
+		format, while "not x1 or not x2 or not x3 will" not work. """
+		self.outputConstraints.append(constraint)
+
+	def addToProfile(self, judgementSet):
+		"""The addToProfile function takes a judgement set as its only argument.
+		A judgement set should be a list of the labels of the formula that are
+		accepted. The rest is rejected. The formulas should be given by their 
+		label and seperated by a semicolon. For example, "4, 2;4;5" """
+		newLabel = len(self.profile)+1
+		formulaLabels = list(map(int, judgementSet.split(";")))
+		acceptedFormulas = []
+		for formulaLabel in formulaLabels:
+			acceptedFormulas.append(self.agenda[formulaLabel])
+		self.profile[newLabel] = acceptedFormulas
+
+	def loadFromFile(self, path):
+		"""Load the scenario from a .jagg file given its path.
+		The path should be a raw string, i.e. of the form r"path/to/file". 
+		The file should have the following format, with each element being on 
+		a new line:
+			- Number of Formulas: The number of formulas in the pre-agenda
+			- X, Formula: The formula labeled by the number X  
+			- In, Formula: The input constraint labeled by the text "In"
+			- Out, Formula: The output constraint labeled by the text "Out"
+			- Number of Judgement Sets: The total number of judgement sets
+			- J, phi_1;...;phi_n: A list of the formulas phi_1 to phi_n 
+				that are accepted labelled by the number J. The rest is rejected.
+				The formulas should be given by their label and seperated 
+				by a semicolon. For example, "4, 2;4;5"
+		A formula should be build by lowercase atoms and the following operators:
+			- not
+			- and 
+			- or
+		The outermost parentheses can be omitted, internal parentheses must
+		be explicit. For example, "(not x1 or not x2) or not x3" is the correct
 		format, while "not x1 or not x2 or not x3 will" not work.
 			"""
+		conn = open(path)
+		text = conn.read()
+		lines = text.splitlines()
+		conn.close()
+
+		# Add the formulas to the agenda dictionary using the given label
+		numberOfFormulas = int(lines[0])
+		for i in range(1, numberOfFormulas+1):
+			currentLine = lines[i].split(", ")
+			label = int(currentLine[0])
+			formula = currentLine[1]
+			self.agenda[label] = formula
+
+		# Add the input constraints to the list of constraints
+		lineNumber = numberOfFormulas+1
+		while lines[lineNumber].split(", ")[0] == "In":
+			formula = lines[lineNumber].split(", ")[1]
+			self.inputConstraints.append(formula)	
+			lineNumber += 1
+		
+		# Add the output constraints to the list of constraints
+		while lines[lineNumber].split(", ")[0] == "Out":
+			formula = lines[lineNumber].split(", ")[1]
+			self.outputConstraints.append(formula)	
+			lineNumber += 1
+			
+		# Add the list of accepted formulas to the profile dictionary
+		# for each of the judgement sets.
+		numberOfJS = int(lines[lineNumber])
+		for i in range(lineNumber+1, lineNumber+numberOfJS+1):
+			currentLine = lines[i].split(", ")
+			label = int(currentLine[0])
+			formulaLabels = list(map(int, currentLine[1].split(";")))
+			acceptedFormulas = []
+			for formulaLabel in formulaLabels:
+				acceptedFormulas.append(self.agenda[formulaLabel])
+			self.profile[label] = acceptedFormulas
+
+	def prettyPrint(self):
+		"""Prints the Scenario object in a readable way"""
 		raise NotImplementedError
 
 # A solver class with an enumerate_outcomes function that enumerates
