14fa9b898362f722914c3e0ef2d26669aca0fb13
Bo Flachs <b.l.flachs@students.uu.nl>
2021-10-22 12:48:16 +0200

    Comment bruteForceSolver.py

--- src/jaggpy/bruteForceSolver.py
+++ src/jaggpy/bruteForceSolver.py
@@ -20,17 +20,21 @@ class BruteForce(Solver):
 			- maxhamming
 			- slater 
 			"""
-		# First we make a list of all the judgement sets that are consistent
-		# with the output constraints.
+		# Translate the agenda so that the issues are represented 
+		# by their labels. These representations will be added to
+		# the constraints.
 		parser = Parser()
 		translatedAgenda = parser.translateAgenda(scenario.agenda)
 
+		# Make a list of all the judgement sets that are consistent
+		# with the output constraints.
 		my_string = ""
 		for conjunct in scenario.outputConstraints:
 			my_string += f'({conjunct}) & '
 		for conjunct in translatedAgenda:
 			my_string += f'({conjunct}) & '
 
+		# Update what variables appear in the scenario
 		allVariables = []
 		for var in scenario.variables:
 			allVariables.append(var)
@@ -42,19 +46,19 @@ class BruteForce(Solver):
 			my_string += f"({var} | ~{var}) & "
 		my_string = my_string[:-3]
 
+		# Preprocess the string representing the scenario
 		var_prefix = "my_var_"
 		my_string_preprocessed = my_string
 		for var in allVariables:
 			my_string_preprocessed = my_string_preprocessed.replace(var, var_prefix + var)
-
 		for var in allVariables:
 			exec(f"{var_prefix}{var} = Var('{var}')")
 		outputConstraint = eval(my_string_preprocessed)
+
+		# List all the models that are consistent with the output constraints
 		consistentOutcomes =  list(outputConstraint.models())
 
-		# We can see if the formula labeled by X is true in model N by using
-		# print(consistentOutcomes[N][scenario.agenda[X]])
-
+		# Depending on the given rule we use helper functions to determine the outcomes
 		# Kemeny rule
 		if rule == "kemeny":
 			print("Computing outcome with brute force and the Kemeny rule...")
@@ -86,6 +90,7 @@ class BruteForce(Solver):
 			
 		# Remove duplicates from outcomes
 		outcomes = [dict(t) for t in {tuple(outcome.items()) for outcome in outcomes}]
+
 		yield outcomes
 
 	def supportNumber(self, agenda, profile):
@@ -103,6 +108,9 @@ class BruteForce(Solver):
 		return supportCount
 
 	def solveKemeny(self, scenario, consistentOutcomes):
+		"""Helper function for the solve function. Given a scenario and the list of
+		consistent outcomes, returns a list of the outcomes corresponding to the
+		Kemeny rule."""
 		# Keep track of the maximum agreement score and initiate list of outcomes
 		maxAgreement = 0
 		outcomes = []
@@ -126,6 +134,9 @@ class BruteForce(Solver):
 		return outcomes	
 
 	def solveMaxHamming(self, scenario, consistentOutcomes):
+		"""Helper function for the solve function. Given a scenario and the list of
+		consistent outcomes, returns a list of the outcomes corresponding to the
+		maxHamming rule."""
 		# Keep track of minimum maximal Hamming distance and initialise list of outcomes.
 		minimumMHD = math.inf
 		outcomes = []
@@ -136,18 +147,13 @@ class BruteForce(Solver):
 			maxHD = 0
 			# Check the Hamming distance from the outcome to each judgement set and track the maximal distance.
 			for judSet in scenario.profile:
-				# Check Hamming distance. NOT CORRECT YET
+				# Check Hamming distance.
 				hamDist = 0
 				for key in scenario.agenda.keys():
 					if outcome[f'l{key}'] and scenario.agenda[key] not in judSet[1]:
 						hamDist += 1
 					elif not outcome[f'l{key}'] and scenario.agenda[key] in judSet[1]: 
 						hamDist += 1
-				# for formula in scenario.agenda.values():
-				# 	if outcome[formula] and formula not in judSet[1]:
-				# 		hamDist += 1
-				# 	elif not outcome[formula] and formula in judSet[1]:
-				# 		hamDist += 1
 
 				# Update the maximum HD for the outcome.
 				if hamDist > maxHD:
@@ -162,6 +168,9 @@ class BruteForce(Solver):
 		return outcomes
 
 	def solveSlater(self, scenario, consistentOutcomes):
+		"""Helper function for the solve function. Given a scenario and the list of
+		consistent outcomes, returns a list of the outcomes corresponding to the
+		Slater rule."""
 		# Determine the set of formulas that has a majority vote, add a 
 		# formula if it is accepted and add it with 'neg ' in front of
 		# it when it is rejected. The 'neg ' is merely a prefix to read
